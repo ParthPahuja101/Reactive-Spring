8cb34773bd9c1d5641d17bb3c498c66734410490
Parth Pahuja <parthpahuja101@gmail.com>
2025-04-23 00:59:35 +0800

    Changes

--- src/main/java/com/reactive/demo/traditionalController/UserTraditionalController.java
+++ src/main/java/com/reactive/demo/traditionalController/UserTraditionalController.java
@@ -3,10 +3,12 @@ package com.reactive.demo.traditionalController;
 import com.reactive.demo.models.User;
 import com.reactive.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,7 +27,11 @@ public class UserTraditionalController {
 
     @GetMapping("/{id}")
     public List<User> getUserById(@PathVariable Integer id){
-        return userService.getUserById(id);
+        List<User> users = userService.getUserById(id);
+        if (users.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return users;
     }
 
 }
